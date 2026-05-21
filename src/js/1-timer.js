a7c5d4a80347c732048fdf61159f83fced4c5e86
Anastasiia Kalyniuk <Kalyniuk.an@gmail.com>
2026-05-21 22:15:44 +0300

    apd +apd css

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -28,6 +28,7 @@ const options = {
       iziToast.error({
         title: 'Error',
         message: 'Please choose a date in the future',
+        position: 'topRight',
       });
 
       return;
